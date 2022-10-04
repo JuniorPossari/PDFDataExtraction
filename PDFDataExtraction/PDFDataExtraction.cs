4acfd14a7652a1766df749e94e0959889e932b23
Junior Possari <junior.possari@pro4tech.com.br>
2022-10-04 12:18:01 -0300

    Add ExtractPagesText

--- PDFDataExtraction/PDFDataExtraction.cs
+++ PDFDataExtraction/PDFDataExtraction.cs
@@ -1,7 +1,47 @@
-﻿namespace PDFDataExtraction
-{
-    public class PDFDataExtraction
-    {
+﻿using iTextSharp.text.pdf;
 
+namespace PDFDataExtraction
+{
+    public static class PDFDataExtraction
+    {
+        /// <summary>
+        /// Extract text from all pages of the PDF file
+        /// </summary>
+        /// <param name="pdfBytes">Bytes of the PDF file</param>
+        /// <returns>Returns a list with the full text of all pages</returns>
+        public static List<string> ExtractPagesText(byte[] pdfBytes)
+        {
+            var pagesText = new List<string>();
+
+            try
+            {
+                var reader = new PdfReader(pdfBytes);
+
+                for (var pageNum = 1; pageNum <= reader.NumberOfPages; pageNum++)
+                {
+                    var contentBytes = reader.GetPageContent(pageNum);
+                    var tokenizer = new PrTokeniser(new RandomAccessFileOrArray(contentBytes));
+
+                    var stringsList = new List<string>();
+
+                    while (tokenizer.NextToken())
+                    {
+                        if (tokenizer.TokenType == PrTokeniser.TK_STRING) stringsList.Add(tokenizer.StringValue);
+                    }
+
+                    var pageText = string.Join("\r\n", stringsList);
+
+                    pagesText.Add(pageText);
+                }
+
+                reader.Close();
+            }
+            catch (Exception)
+            {
+                pagesText = new List<string>();
+            }
+
+            return pagesText;
+        }
     }
 }
